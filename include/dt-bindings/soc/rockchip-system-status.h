db376ec7f691c8208bd7510cb8385342aa4fb9e8
imchina001-leo <78472000@qq.com>
2026-03-31 17:57:10 +0800

    Update rockchip-system-status.h

--- include/dt-bindings/soc/rockchip-system-status.h
+++ include/dt-bindings/soc/rockchip-system-status.h
@@ -11,31 +11,31 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _DT_BINDINGS_SOC_ROCKCHIP_SYSTEM_STATUS_H
-#define _DT_BINDINGS_SOC_ROCKCHIP_SYSTEM_STATUS_H
+#ifndef _RK_DT_BINDINGS_SOC_ROCKCHIP_SYSTEM_STATUS_H
+#define _RK_DT_BINDINGS_SOC_ROCKCHIP_SYSTEM_STATUS_H
 
-#define RK_SYS_STATUS_NORMAL		(1 << 0)
-#define RK_SYS_STATUS_SUSPEND		(1 << 1)
-#define RK_SYS_STATUS_IDLE		(1 << 2)
-#define RK_SYS_STATUS_REBOOT		(1 << 3)
-#define RK_SYS_STATUS_VIDEO_4K		(1 << 4)
-#define RK_SYS_STATUS_VIDEO_1080P	(1 << 5)
-#define RK_SYS_STATUS_GPU		(1 << 6)
-#define RK_SYS_STATUS_RGA		(1 << 7)
-#define RK_SYS_STATUS_CIF0		(1 << 8)
-#define RK_SYS_STATUS_CIF1		(1 << 9)
-#define RK_SYS_STATUS_LCDC0		(1 << 10)
-#define RK_SYS_STATUS_LCDC1		(1 << 11)
-#define RK_SYS_STATUS_BOOST		(1 << 12)
-#define RK_SYS_STATUS_PERFORMANCE	(1 << 13)
-#define RK_SYS_STATUS_ISP		(1 << 14)
-#define RK_SYS_STATUS_HDMI		(1 << 15)
-#define RK_SYS_STATUS_VIDEO_4K_10B	(1 << 16)
-#define RK_SYS_STATUS_LOW_POWER		(1 << 17)
+#define SYS_STATUS_NORMAL		(1 << 0)
+#define SYS_STATUS_SUSPEND		(1 << 1)
+#define SYS_STATUS_IDLE			(1 << 2)
+#define SYS_STATUS_REBOOT		(1 << 3)
+#define SYS_STATUS_VIDEO_4K		(1 << 4)
+#define SYS_STATUS_VIDEO_1080P		(1 << 5)
+#define SYS_STATUS_GPU			(1 << 6)
+#define SYS_STATUS_RGA			(1 << 7)
+#define SYS_STATUS_CIF0			(1 << 8)
+#define SYS_STATUS_CIF1			(1 << 9)
+#define SYS_STATUS_LCDC0		(1 << 10)
+#define SYS_STATUS_LCDC1		(1 << 11)
+#define SYS_STATUS_BOOST		(1 << 12)
+#define SYS_STATUS_PERFORMANCE		(1 << 13)
+#define SYS_STATUS_ISP			(1 << 14)
+#define SYS_STATUS_HDMI			(1 << 15)
+#define SYS_STATUS_VIDEO_4K_10B		(1 << 16)
+#define SYS_STATUS_LOW_POWER		(1 << 17)
 
-#define RK_SYS_STATUS_VIDEO		(RK_SYS_STATUS_VIDEO_4K | \
-					 RK_SYS_STATUS_VIDEO_1080P | \
-					 RK_SYS_STATUS_VIDEO_4K_10B)
-#define RK_SYS_STATUS_DUALVIEW		(RK_SYS_STATS_LCDC0 | RK_SYS_STATUS_LCDC1)
+#define SYS_STATUS_VIDEO		(SYS_STATUS_VIDEO_4K | \
+					 SYS_STATUS_VIDEO_1080P | \
+					 SYS_STATUS_VIDEO_4K_10B)
+#define SYS_STATUS_DUALVIEW		(SYS_STATUS_LCDC0 | SYS_STATUS_LCDC1)
 
 #endif
